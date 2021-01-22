71ca6f7b2cf26f7cd17021e7a8af3afe8d669f60
alexandr05shev4enko <alex050798@gmail.com>
2021-01-22 23:44:08 +0200

    added serach line

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,13 @@ $(function(){
     smallBtn:true
   });
 
+  $(".search-icon").on("click", function(){
+    $(".search-modal").addClass("search-modal--active")
+  });
+
+  $(".search-modal__cross").on("click", function(){
+    $(".search-modal").removeClass("search-modal--active")
+  });
+
 
 });
