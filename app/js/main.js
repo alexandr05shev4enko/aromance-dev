3ca992721a854f24588c9cac73f21d5a18891879
Alexandr Shevchenko <alex050798@gmail.com>
2021-01-25 11:31:49 +0200

    added section featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,14 @@
 $(function(){
 
-  $('.featured__slider').slick({
-    // dots: true,
-    // arrows: true,
-    // autoplay: false,
-    // autoplaySpeed: 2000,
-    // slidesToShow: 1,
-    // slidesToScroll: 1
+  
+
+  $(".featured__slider-items").slick({
+    arrows: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    // centerMode: true,
+    dots: true,
+    swipe: false
   });
 
   $("[data-fancybox]").fancybox({
@@ -21,8 +23,32 @@ $(function(){
     $(".search-modal").removeClass("search-modal--active")
   });
 
-  
- 
+  // var currentSlide = $('.featured__slider-items').slick('slickCurrentSlide');
 
+  // if(currentSlide){
+  //   console.log('it`s work');
+  //   $(".featured__slider-item").addClass("featured__slider-item--active");
+    
+  // }
+
+
+  // $('.your-element').on('afterChange', function(event, slick, currentSlide){
+  //   console.log(currentSlide);
+  // }); 
+
+  // $('.featured__slider-items').on('afterChange', function(event, slick, currentSlide){
+  //   $('.slick-active p').addClass('visible');
+  //   $('.slick-active p').removeClass('hidden');
+  // });
+
+  // $('.featured__slider-items').on('beforeChange', function(event, slick, currentSlide){
+  //   $('.slick-active p').removeClass('visible');
+  //   $('.slick-active p').addClass('hidden');
+  // });
+
+  // $(".featured__slider-items .featured__slider-item").on("click", function() {
+  //   const index = $(this).attr("data-slick-index");
+  //   $(".featured__slider-items").slick("slickGoTo", index);
+  // });
 
 });
