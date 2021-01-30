b57ca3cec04dfb7b94b0f09a1ab0ec13dc4e36e4
Alexandr Shevchenko <alex050798@gmail.com>
2021-01-30 15:14:10 +0200

    finish product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,20 @@
 $(function(){
 
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 9,
-    slidesToScroll: 0,
-    asNavFor: '.slider-for',
-    dots: false,
-    arrows: false,
-    centerMode: true,
-    focusOnSelect: true,
-    rows: 3,
-    slidesPerRow: 3
-  });
+  // $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav'
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   dots: false,
+  //   arrows: false,
+  //   focusOnSelect: true,
+  // });
 
   $(".reviews__items").slick({
     vertical: true,
